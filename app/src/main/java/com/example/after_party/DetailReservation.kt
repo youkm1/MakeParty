833214c9c8a52dbc4dbaac3a30a00269888339cf
윤서희 <appleid21@sookmyung.ac.kr>
2023-11-27 23:11:59 +0900

    Feat: 구글맵 세팅

---
+++ app/src/main/java/com/example/after_party/DetailReservation.kt
@@ -0,0 +1,11 @@
+package com.example.after_party
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailReservation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_reservation)
+    }
+}
