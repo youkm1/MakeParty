bd8e11bad115366ace446e7d230eb622a2f50289
윤서희 <appleid21@sookmyung.ac.kr>
2023-11-28 12:50:20 +0900

    Feat: #5 상세페이지 xml 구현
    구글맵 api 사용

--- app/src/main/java/com/example/after_party/DetailReservation.kt
+++ app/src/main/java/com/example/after_party/DetailReservation.kt
@@ -2,10 +2,32 @@ package com.example.after_party
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class DetailReservation : AppCompatActivity() {
+class DetailReservation : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    private val storeLat = 37.54276779 // 가게의 위도
+    private val storeLng = 126.97374716 // 가게의 경도
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_reservation)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val storeLocation = LatLng(storeLat, storeLng)
+        mMap.addMarker(MarkerOptions().position(storeLocation).title("가게 이름"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(storeLocation, 15f))
     }
 }
