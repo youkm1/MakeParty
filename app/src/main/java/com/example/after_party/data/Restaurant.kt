299ae2f49b07e695b5a86f3a5bbf38c861a46d88
yu-k-m <77780624+youkm1@users.noreply.github.com>
2023-12-05 19:52:00 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/after_party/data/Restaurant.kt
@@ -0,0 +1,9 @@
+package com.example.after_party.data
+
+import android.net.Uri
+
+data class Restaurant(
+    val name:String,
+    val address:String,
+
+)
---
+++ app/src/main/java/com/example/after_party/data/User.kt
@@ -0,0 +1,9 @@
+package com.example.after_party.data
+
+data class User(
+    val email: String? = null,
+    val phone: String? = null,
+    val password: String? = null,
+    val isLogined: Boolean? = false
+
+)
