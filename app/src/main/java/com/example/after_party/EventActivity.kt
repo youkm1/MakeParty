d5bf7d6459f70953a5b7c795a28faf2fd66e561d
yu-k-m <karen0806@naver.com>
2023-12-10 02:37:46 +0900

    working on GPS permission

--- app/src/main/java/com/example/after_party/EventActivity.kt
+++ app/src/main/java/com/example/after_party/EventActivity.kt
@@ -1,8 +1,12 @@
 package com.example.after_party
 
+import android.media.Image
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.cardview.widget.CardView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 
 
 class EventActivity : ComponentActivity() {
@@ -29,9 +34,17 @@ class EventActivity : ComponentActivity() {
     }
 }
 
+
 @Preview
 @Composable
 fun EventScreen(){
+    AndroidView(factory = { View.inflate(it,R.layout.activity_event_page,null) },
+        modifier=Modifier.fillMaxSize(),
+        update = {
+
+        }
+
+    )
 
 }
 
---
+++ app/src/main/java/com/example/after_party/SearchModalFragment.kt
@@ -0,0 +1,24 @@
+package com.example.after_party
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class SearchModalFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                //modal()
+            }
+        }
+    }
+}
+
+
