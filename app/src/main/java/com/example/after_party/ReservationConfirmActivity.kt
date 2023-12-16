770b849c42b7bf24f9e028d09853296120564be7
윤서희 <appleid21@sookmyung.ac.kr>
2023-12-16 16:06:05 +0900

    Feat: 예약 완료 페이지

---
+++ app/src/main/java/com/example/after_party/ReservationConfirmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.after_party
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReservationConfirmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation_confirm)
+    }
+}
